8155f43e79900aa7982217d0c34c487433aac52a
vivekkadoli <vivekkadoli@gmail.com>
2025-05-19 13:28:57 +0530

    Form Designed successfully for generating query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './index.module.css';
+import sqlLogo from './assets/sql-logo.png';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryDescription, setQueryDescription] = useState('');
+
+  // const onSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const response = await fetch('http://localhost:5000/generate-sql', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ queryDescription })
+  //   });
+
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Query Description:', queryDescription);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className={styles.main}>
+      <img src={sqlLogo} alt="" className={styles.icon}/>
+      <h3>Generate SQL Query with AI</h3>
+      <form onSubmit={onSubmit}>
+        <input 
+        type="text" 
+        name='query-description'
+        placeholder='Describe your SQL query here'
+        onChange={(e) => setQueryDescription(e.target.value)}
+        />
+        <input type="submit" value="Generate SQL Query"/>
+      </form>
+    </main>
   )
 }
 
